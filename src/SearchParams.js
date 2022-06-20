afbda5bf1278150ca66eb6ef3724fc0d89ea081d
Miloš Ergić <milos.ergic@nultien.rs>
2022-06-20 17:49:58 +0200

    Search form component

---
+++ src/SearchParams.js
@@ -0,0 +1,17 @@
+const SearcParams = () => {
+    const location = "Seattle, WA";
+    return (
+        <div className="search-params">
+            <form>
+                <label htmlFor="location">
+                    Location
+                    <input id="location" value={location}
+                    placeholder="Location"> 
+                    </input>    
+                </label>
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+export default SearcParams;
